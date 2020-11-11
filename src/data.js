740e20ec7f311b1cb300b15e35b303e77d33cb46
Ivan Skovpin <ivanskovpin@gmail.com>
2020-11-11 20:32:44 +0700

    + getCellUrl function

--- src/data.js
+++ src/data.js
@@ -1,20 +1,20 @@
 function addRecord() {
   var ss = SpreadsheetApp.getActiveSpreadsheet();
   var sheet = ss.getActiveSheet();
-  
+
   var srcRowNum = getSampleDataRow();
   var destRowNum = srcRowNum + 1;
   sheet.insertRowAfter(srcRowNum);
-  
+
   var srcRow = getRecord(srcRowNum);
-  var destRow = getRecord(destRowNum);  
-  
+  var destRow = getRecord(destRowNum);
+
   srcRow.copyTo(destRow);
-  
+
   var date = Utilities.formatDate(new Date(), "GMT+7", "dd.MM.yyyy");
   var dateCell = getDateCell(destRowNum);
   dateCell.setValue(date);
-  
+
   var sumCell = getSumCell(destRowNum);
   sumCell.activate().setValue("");
 }
@@ -24,11 +24,11 @@ function getRecord(row) {
 }
 
 function getFirstDataRow() {
-  return 4; 
+  return 4;
 }
 
 function getSampleDataRow() {
-  return 3; 
+  return 3;
 }
 
 function getDateCell(row) {
@@ -51,7 +51,19 @@ function getInfoCell(row) {
   return SpreadsheetApp.getActiveSpreadsheet().getRange("E" + row);
 }
 
+/**
+ * @param {GoogleAppsScript.Spreadsheet.Range} range
+ * @param {GoogleAppsScript.Spreadsheet.Spreadsheet} ss
+ * @param {GoogleAppsScript.Spreadsheet.Sheet} sheet
+ * @return {string}
+ */
+function getCellUrl(range, ss, sheet) {
+    if (ss === undefined) {
+        ss = SpreadsheetApp.getActiveSpreadsheet();
+    }
+    if (sheet === undefined) {
+        sheet = ss.getActiveSheet();
+    }
 
-
-
-
+    return ss.getUrl() + '#gid=' + sheet.getSheetId() + "&range=" + range.getA1Notation();
+}
