ce55cb8c7cdfaca5d3e15af64feb15e9b7b7b637
Ivan Skovpin <ivanskovpin@gmail.com>
2020-11-30 15:15:37 +0700

    + kto-to in dictionary

--- src/dictionary.js
+++ src/dictionary.js
@@ -125,6 +125,7 @@ class Verb {
  * @type {Dictionary}
  */
 let dict = new Dictionary();
+dict.addNoun(new Noun('кто-то', 'кого-то', 'кому-то', 'кого-то', 'кем-то', 'о ком-то', VerbCase.MALE));
 dict.addNoun(new Noun('Иван', 'Ивана', 'Ивану', 'Ивана', 'Иваном', 'об Иване', VerbCase.MALE));
 dict.addNoun(new Noun('Арсения', 'Арсении', 'Арсении', 'Арсению', 'Арсенией', 'об Арсении', VerbCase.FEMALE));
 dict.addNoun(new Noun('Александр', 'Александра', 'Александру', 'Александра', 'Александром', 'об Александре', VerbCase.MALE));
--- src/log.js
+++ src/log.js
@@ -16,21 +16,22 @@ function logNewRecord(e) {
   if (!sheetInfo.isInfoCell(range) || e.oldValue !== undefined) {
     return;
   }
-  
+
   var row = range.getRow();
   var who = getWhoCell(row).getValue();
   var sum = getSumCell(row).getValue().toFixed(2);
   var info = getInfoCell(row).getValue();
 
-  if (!who) {
-    who = 'Кто-то';
-  }
+    if (!who) {
+        who = dict.getNoun('кто-то', NounCase.IM);
+    }
 
   let infoLink = '<a href="' + getCellUrl(range, e.source, e.source.getActiveSheet()) + '">' + info + '</a>';
-  var msg = who + " " + dict.getVerb('добавить', dict.getNounGender(who)) + " новую запись: " + sum + " руб. за " + infoLink;
+  let msg = who + " " + dict.getVerb('добавить', dict.getNounGender(who)) + " новую запись: " + sum + " руб. за " + infoLink;
+  msg = msg.capitalize();
 
   Logger.log(msg);
-  tgBotSendMessage(msg);  
+  tgBotSendMessage(msg);
 }
 
 /**
@@ -41,28 +42,29 @@ function logPayment(e) {
   if (!sheetInfo.isCheckPaymentCell(e.range)) {
     return;
   }
-  
+
   var ss = SpreadsheetApp.getActiveSpreadsheet();
   var sheet = ss.getActiveSheet();
 
   var range = e.range;
   var col = range.getColumn();
   var row = range.getRow();
-  
+
   var resultCell = sheet.getRange(row, col, 1);
-  var partCell = sheet.getRange(row, col - 1, 1);  
+  var partCell = sheet.getRange(row, col - 1, 1);
   var who = getWhoCell(row).getValue();
   var info = getInfoCell(row).getValue();
   var fromWho = sheet.getRange(2, col - 1, 1).getValue();
   var howMuch = partCell.getValue().toFixed(2);
-  
+
   if (!resultCell.getValue() || !partCell.getValue() || who === fromWho) {
     return;
   }
 
   let infoLink = '<a href="' + getCellUrl(getInfoCell(row), e.source, e.source.getActiveSheet()) + '">' + info + '</a>';
   var msg = fromWho + " " + dict.getVerb('заплатить', dict.getNounGender(fromWho)) + " " + howMuch + " руб. " + dict.getNoun(who, NounCase.DAT)  + " за " + infoLink;
+  msg = msg.capitalize();
 
-  Logger.log(msg);
-  tgBotSendMessage(msg);    
+    Logger.log(msg);
+  tgBotSendMessage(msg);
 }
