5af3a862d84d7f520ee54ca265c7ee9827f46601
Ivan Skovpin <ivanskovpin@gmail.com>
2020-12-03 01:05:18 +0700

    + a few useful functions

---
+++ src/functions.js
@@ -0,0 +1,60 @@
+/**
+ * @param {Object} object
+ * @return {number|undefined}
+ */
+function getObjectMaxRecursive (object) {
+    let max;
+    for (let key in object) {
+        let elemMax;
+        if (typeof object[key] === 'object') {
+            elemMax = getObjectMaxRecursive(object[key]);
+        } else if (typeof object[key] === 'number') {
+            elemMax = object[key];
+        }
+
+        if (elemMax !== undefined && (max === undefined || elemMax > max)) {
+            max = elemMax;
+        }
+    }
+
+    return max;
+}
+
+/**
+ * @param {Object} object
+ * @return {number|undefined}
+ */
+function getObjectMinRecursive (object) {
+    let min;
+    for (let key in object) {
+        let elemMin;
+        if (typeof object[key] === 'object') {
+            elemMin = getObjectMinRecursive(object[key]);
+        } else if (typeof object[key] === 'number') {
+            elemMin = object[key];
+        }
+
+        if (elemMin !== undefined && (min === undefined || elemMin < min)) {
+            min = elemMin;
+        }
+    }
+
+    return min;
+}
+
+/**
+ * @param {GoogleAppsScript.Spreadsheet.Range} range
+ * @param {GoogleAppsScript.Spreadsheet.Spreadsheet} ss
+ * @param {GoogleAppsScript.Spreadsheet.Sheet} sheet
+ * @return {string}
+ */
+function getCellUrl(range, ss, sheet) {
+    if (ss === undefined) {
+        ss = SpreadsheetApp.getActiveSpreadsheet();
+    }
+    if (sheet === undefined) {
+        sheet = ss.getActiveSheet();
+    }
+
+    return ss.getUrl() + '#gid=' + sheet.getSheetId() + "&range=" + range.getA1Notation();
+}
