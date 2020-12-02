8be302d503665d8253892fa1eb9e8c0d56391172
Ivan Skovpin <ivanskovpin@gmail.com>
2020-12-03 01:03:00 +0700

    * expenses data manipulating in OOP style

--- src/data.js
+++ src/data.js
@@ -1,69 +1,240 @@
+/**
+ * Add new expense record
+ */
 function addRecord() {
-    var ss = SpreadsheetApp.getActiveSpreadsheet();
-    var sheet = ss.getActiveSheet();
+    let sheetInfo = new ExpensesSheetInfo();
+    let sheet = sheetInfo.getSheet();
 
-    var srcRowNum = getSampleDataRow();
-    var destRowNum = srcRowNum + 1;
-    sheet.insertRowAfter(srcRowNum);
+    let srcRowNum = sheetInfo.sampleDataRow;
+    let destRowNum = sheetInfo.firstDataRow;
+    sheet.insertRowBefore(destRowNum);
 
-    var srcRow = getRecord(srcRowNum);
-    var destRow = getRecord(destRowNum);
+    let firstColumn = sheetInfo.getFirstConfigurableColumn();
+    let lastColumn = sheetInfo.getLastConfigurableColumn();
+    let columnsNumber = lastColumn - firstColumn + 1;
+
+    let srcRow = sheet.getRange(srcRowNum, firstColumn, 1, columnsNumber);
+    let destRow = sheet.getRange(destRowNum, firstColumn, 1, columnsNumber);
 
     srcRow.copyTo(destRow);
 
-    var date = Utilities.formatDate(new Date(), "GMT+7", "dd.MM.yyyy");
-    var dateCell = getDateCell(destRowNum);
-    dateCell.setValue(date);
-
-    var sumCell = getSumCell(destRowNum);
-    sumCell.activate().setValue("");
+    let record = new ExpenseRecord(destRowNum, sheetInfo);
+    let date = Utilities.formatDate(new Date(), config.get('timezone'), config.get('dateFormat'));
+    record.setDate(date);
+    record.setSum('');
+    record.sumCell.activate();
 }
 
-function getRecord(row) {
-    return SpreadsheetApp.getActiveSpreadsheet().getRange("A" + row + ":S" + row);
-}
-
-function getFirstDataRow() {
-    return 4;
-}
-
-function getSampleDataRow() {
-    return 3;
-}
-
-function getDateCell(row) {
-    return SpreadsheetApp.getActiveSpreadsheet().getRange("A" + row);
-}
-
-function getSumCell(row) {
-    return SpreadsheetApp.getActiveSpreadsheet().getRange("B" + row);
-}
-
-function getDebtCell(row) {
-    return SpreadsheetApp.getActiveSpreadsheet().getRange("C" + row);
-}
-
-function getWhoCell(row) {
-    return SpreadsheetApp.getActiveSpreadsheet().getRange("D" + row);
-}
-
-function getInfoCell(row) {
-    return SpreadsheetApp.getActiveSpreadsheet().getRange("E" + row);
-}
 
 /**
- * @param {GoogleAppsScript.Spreadsheet.Range} range
- * @param {GoogleAppsScript.Spreadsheet.Spreadsheet} ss
- * @param {GoogleAppsScript.Spreadsheet.Sheet} sheet
- * @return {string}
+ * Common record class. Only for extending.
  */
-function getCellUrl(range, ss, sheet) {
-    if (ss === undefined) {
-        ss = SpreadsheetApp.getActiveSpreadsheet();
-    }
-    if (sheet === undefined) {
-        sheet = ss.getActiveSheet();
+class Record {
+    /**
+     * @param {number} row
+     * @param {ExpensesSheetInfo|DebtsSheetInfo} sheetInfo
+     */
+    constructor(row, sheetInfo) {
+        this.row = row;
+        this.sheetInfo = sheetInfo;
     }
 
-    return ss.getUrl() + '#gid=' + sheet.getSheetId() + "&range=" + range.getA1Notation();
+    /**
+     * @param {number} column
+     * @return {string}
+     */
+    getCellValue(column) {
+        return this.getCell(column).getValue();
+    }
+
+    /**
+     *
+     * @param {number} column
+     * @param {number?} row
+     * @return {GoogleAppsScript.Spreadsheet.Range}
+     */
+    getCell(column, row) {
+        if (row === undefined) {
+            row = this.row;
+        }
+        return this.sheetInfo.getSheet().getRange(row, column);
+    }
+}
+
+class DebtRecord extends Record {
+    /**
+     * @param {number} row
+     * @param {ExpensesSheetInfo} sheetInfo
+     * @param {Object} debtorConfig
+     */
+    constructor(row, sheetInfo, debtorConfig) {
+        super(row, sheetInfo);
+        this.debtorConfig = debtorConfig;
+
+        this.nameCell = this.getCell(debtorConfig.name, sheetInfo.tableHeaderRow);
+        this.sumCell = this.getCell(debtorConfig.sum);
+        this.checkCell = this.getCell(debtorConfig.check);
+        this.participantCell = this.getCell(debtorConfig.participant);
+    }
+
+    /**
+     * @return {string}
+     */
+    getName() {
+        return this.nameCell.getValue();
+    }
+
+    /**
+     * @return {number}
+     */
+    getSum() {
+        return parseFloat(this.sumCell.getValue());
+    }
+
+    /**
+     * @return {boolean}
+     */
+    getCheck() {
+        return Boolean(this.checkCell.getValue());
+    }
+
+    /**
+     * @return {boolean}
+     */
+    getIsParticipant() {
+        return Boolean(this.participantCell.getValue());
+    }
+
+    /**
+     * @param {boolean} value
+     * @return {DebtRecord}
+     */
+    setCheck(value) {
+        this.checkCell.setValue(value);
+        return this;
+    }
+
+    /**
+     * @param {boolean} value
+     * @return {DebtRecord}
+     */
+    setIsParticipant(value) {
+        this.participantCell.setValue(value);
+        return this;
+    }
+}
+
+class ExpenseRecord extends Record {
+
+    /**
+     * @param {number} row
+     * @param {ExpensesSheetInfo} sheetInfo
+     */
+    constructor(row, sheetInfo) {
+        super(row, sheetInfo);
+
+        this.dateCell = this.getCell(this.sheetInfo.columnDate);
+        this.sumCell = this.getCell(this.sheetInfo.columnExpenseSum);
+        this.sumDebtCell = this.getCell(this.sheetInfo.columnDebt);
+        this.payerCell = this.getCell(this.sheetInfo.columnPayer);
+        this.infoCell = this.getCell(this.sheetInfo.columnInfo);
+
+        this.debtRecords = {};
+        this.sheetInfo.debtorsColumns.forEach(debtorColumns => {
+                let debtRecord = new DebtRecord(this.row, this.sheetInfo, debtorColumns);
+                this.debtRecords[debtRecord.getName()] = debtRecord;
+            }
+        );
+    }
+
+    /**
+     * @return {string}
+     */
+    getDate() {
+        return this.dateCell.getValue();
+    }
+
+    /**
+     * @return {number}
+     */
+    getSum() {
+        return parseFloat(this.sumCell.getValue());
+    }
+
+    /**
+     * @return {number}
+     */
+    getSumDebt() {
+        return parseFloat(this.sumDebtCell.getValue());
+    }
+
+    /**
+     * @return {string}
+     */
+    getPayer() {
+        return this.payerCell.getValue();
+    }
+
+    /**
+     * @return {string}
+     */
+    getInfo() {
+        return this.infoCell.getValue();
+    }
+
+    /**
+     * @return {Object}
+     */
+    getDebtRecords() {
+        return this.debtRecords;
+    }
+
+    /**
+     * @param {String} debtorName
+     * @return {DebtRecord}
+     */
+    getDebtRecordByName(debtorName) {
+        let record = this.debtRecords[debtorName];
+        if (record === undefined) {
+            throw 'Debtor ' + debtorName + ' not found';
+        }
+
+        return record;
+    }
+
+    /**
+     * @param {string} value
+     * @return {ExpenseRecord}
+     */
+    setDate(value) {
+        this.dateCell.setValue(value);
+        return this;
+    }
+
+    /**
+     * @param {number} value
+     * @return {ExpenseRecord}
+     */
+    setSum(value) {
+        this.sumCell.setValue(value);
+        return this;
+    }
+
+    /**
+     * @param {number} value
+     * @return {ExpenseRecord}
+     */
+    setPayer(value) {
+        this.payerCell.setValue(value);
+        return this;
+    }
+
+    /**
+     * @param {number} value
+     * @return {ExpenseRecord}
+     */
+    setInfo(value) {
+        this.infoCell.setValue(value);
+        return this;
+    }
 }
