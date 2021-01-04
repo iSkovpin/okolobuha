29719b61882d2f6426248eeda1a0a5b90b1fa85d
Ivan Skovpin <ivanskovpin@gmail.com>
2021-01-04 21:10:27 +0700

    * less initialization code

--- src/init.js
+++ src/init.js
@@ -4,19 +4,13 @@ if (isDev()) {
 
 /**
  * Config initialization.
+ * todo it can be encapsulated in okolobuha object too.
  * @type {Config}
  */
 let config = new Config(isDev() ? configDev : configProd);
 
 /**
- * Dictionary initialization.
- * @type {Dictionary}
+ * Main app object.
+ * @type {OkolobuhaApp}
  */
-let dict = new Dictionary();
-dict.addNoun(new Noun('кто-то', 'кого-то', 'кому-то', 'кого-то', 'кем-то', 'о ком-то', VerbCase.MALE));
-dict.addNoun(new Noun('Иван', 'Ивана', 'Ивану', 'Ивана', 'Иваном', 'об Иване', VerbCase.MALE));
-dict.addNoun(new Noun('Арсения', 'Арсении', 'Арсении', 'Арсению', 'Арсенией', 'об Арсении', VerbCase.FEMALE));
-dict.addNoun(new Noun('Александр', 'Александра', 'Александру', 'Александра', 'Александром', 'об Александре', VerbCase.MALE));
-dict.addNoun(new Noun('Яна', 'Яны', 'Яне', 'Яну', 'Яной', 'о Яне', VerbCase.FEMALE));
-dict.addVerb(new Verb('заплатить', 'заплатил', 'заплатила', 'заплатило'));
-dict.addVerb(new Verb('добавить', 'добавил', 'добавила', 'добавило'));
+let okolobuha = new OkolobuhaApp(config);
